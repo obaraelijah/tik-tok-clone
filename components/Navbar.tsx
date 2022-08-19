1fff30f3c7558a146134e587a098fbe596bf41bd
elijah obara <elijahobara357@gmail.com>
2022-08-19 22:31:19 +0300

    front-end part

---
+++ components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import { GoogleLogin, googleLogout  } from '@react-oauth/google';
+import Logo from '../utils/tiktik-logo.png'
+import { createOrGetUser } from '../utils';
+
+
+const Navbar = () => {
+  const user = false;
+  return (
+    <div  className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+       <Link href='/'>
+        <div className='w-[100px] md:w-[129px] '>
+          <Image
+            className='cursor-pointer'
+            src={Logo}
+            alt='TIKTIK'
+            layout='responsive'
+          />
+        </div>
+      </Link>
+
+      <div>Search</div>
+
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin 
+           onSuccess={(response) => createOrGetUser(response) }
+           onError={() => console.log('ERror')}
+          /> 
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
